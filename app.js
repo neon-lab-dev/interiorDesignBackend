9b2afd99246552a507a5f86b90dd880b3fbce4ae
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-11-25 21:33:36 +0545

    fix:cors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
 
 app.use(
   cors({
-    origin: ["*", "http://localhost:3000"],
+    origin: ["*", "http://localhost:3000", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PUT"],
   })

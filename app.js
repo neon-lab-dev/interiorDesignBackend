96e64958893095f3ae367bcd91e13155b6162a49
neon-lab-dev <162788012+neon-lab-dev@users.noreply.github.com>
2024-12-10 17:44:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
 
 app.use(
   cors({
-    origin: ["*", "http://localhost:3000", "http://localhost:5173"],
+    origin: ["*", "http://localhost:3000", "http://localhost:5173","https://interiordesign-wheat.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PUT"],
   })
